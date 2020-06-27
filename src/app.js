b96e335115d3782846355c772d115c8067d9cfda
cod5000 <cod5000@gmail.com>
2020-06-27 17:49:02 -0400

    Server fix

--- src/app.js
+++ src/app.js
@@ -76,7 +76,7 @@ app.get('*', (req, res) => {
   res.redirect('/');
 })
 
-const port = '3000' || process.env.PORT;
+const port = process.env.PORT;
 app.listen(port, () => {
   console.log(`Listening on ${port}`)
 });
